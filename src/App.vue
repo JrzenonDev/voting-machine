5253e65ce3585e3298f4c520e5c252b61757b5ff
JrzenonDev <jrobertoonb@gmail.com>
2021-07-16 00:32:21 -0300

    chore: adjust general

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,40 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <h1>Welcome to Your Vue.js App</h1>
+    <div class="urna">
+      <Teclado />
+    </div>
   </div>
 </template>
+
+<script>
+import Teclado from '@/components/Teclado.vue';
+
+import '@/css/globals.css';
+
+export default {
+  name: 'App',
+  components: {
+    Teclado
+  }
+}
+</script>
+
+<style scoped>
+  #app {
+    background-color: var(--background-color);
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .urna {
+    width: 1000px;
+    height: 500px;
+    background-color: var(--ballot-box-background-color);
+    padding: 30px;
+    border-radius: 5px;
+    display: flex;
+  }
+</style>
